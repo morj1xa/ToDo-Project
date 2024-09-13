685e43465da3428985a05b7fea52aff2cad33d6e
morj1xa <seva.koshelev05@mail.ru>
2024-09-13 21:00:49 +0300

    Component 'Header' added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import Header from "./Components/Header";
 import "./App.css";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
